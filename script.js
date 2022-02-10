dc3889f5e623bd6784c3a257a8270a784e7cc3fa
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-10 07:46:45 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("wordle").innerHTML = "Apple";
+let randomWord = ["Apple", "Ready", "Death"];
+
+let randomIndex = Math.random() * (5 - 0)
+randomFlt = randomIndex.toFixed()
+alert(randomFlt)
