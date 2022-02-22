564c682f5a466f2d0cf9bb2420110b728cd285e8
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-22 08:44:24 -0700

    -

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let bBtn = document.getElementById("bBtn");
 let cBtn = document.getElementById("cBtn");
 let buttonClicked;
 let submitOrder = [firstGuess, secondGuess, thirdGuess, fourthGuess, fifthGuess];
-let submitLoop  = [0, 1, 2, 3, 4];
+//let submitLoop  = [0, 1, 2, 3, 4];
 let letterCheck = 0;
-let splicedSubmitLoop = submitLoop.splice(letterCheck, 1)
+//let splicedSubmitLoop = submitLoop.splice(letterCheck, 1)
 let possibleWords = ["APPLE", "READY", "DEATH", "AUDIO", "BOILS"];
 let randomInt = Math.random() * (4 - 0);
 let randomFlt = randomInt.toFixed();
@@ -54,20 +54,22 @@ function guess() {
 
 function submit() {
     if (numberOfGuesses === 5) {
-        for (letterCheck; letterCheck <= 4; letterCheck ++) { 
+        for (letterCheck; letterCheck <= 4; letterCheck ++) {
+            let submitLoop = [0, 1, 2, 3, 4];
+            submitLoop.splice(letterCheck, 1);
             if (submitOrder[letterCheck].innerHTML === randomWord[letterCheck]) {  
                 submitOrder[letterCheck].style.backgroundColor = "rgb(0, 255, 0)" 
             } else if (submitOrder[letterCheck].innerHTML === randomWord[submitLoop[0]] || submitOrder[letterCheck].innerHTML === randomWord[submitLoop[1]] || submitOrder[letterCheck].innerHTML === randomWord[submitLoop[2]] || submitOrder[letterCheck].innerHTML === randomWord[submitLoop[3]]) {
                 let wordIndex = randomWord.indexOf(submitOrder[letterCheck].innerHTML)
                 if (submitOrder[submitLoop[0]].innerHTML === randomWord[wordIndex] || submitOrder[submitLoop[1]].innerHTML === randomWord[wordIndex] || submitOrder[submitLoop[2]].innerHTML === randomWord[wordIndex] || submitOrder[submitLoop[3]].innerHTML === randomWord[wordIndex]) {
-                    alert("EEE")
                     submitOrder[letterCheck].style.backgroundColor = "grey"
                 } else {
                     submitOrder[letterCheck].style.backgroundColor = "rgb(255, 255, 0)"
                 }
             } else {
                 submitOrder[letterCheck].style.backgroundColor = "grey"
-            } 
+            }
         }
     }
 }   
+
